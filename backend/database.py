a60a09165e201797d98ea8fd4a77ee5b1ded7b7a
Pranavseelam06 <pseelam@purdue.edu>
2026-01-17 13:24:42 -0500

    SQL DATABASE UPDATE

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./lms.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  # needed for SQLite
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
